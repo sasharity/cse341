63a3e0db9d6c3f760a7648d6c721e184180d75d5
sasharity <iruogheneomaraye@gmail.com>
2026-03-06 03:53:24 +0100

    Add CSE341 project files without submodules

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// const { createServer } = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+ 
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
+
